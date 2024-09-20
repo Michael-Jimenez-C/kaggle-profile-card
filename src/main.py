8935f55b58581ae29bf7217dff99dff39d915663
Michael Jimenez <michaeljimenezcontreras1@gmail.com>
2024-09-19 21:37:12 -0500

    static folder and files for tier

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Response
+from fastapi.staticfiles import StaticFiles
+
 from scrapper import getUser
 from svg import svg
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 @app.get('/{username}')
 async def userCard(username: str):
+    """
+    Endpoint to extract the user data and generate the svg image
+    """
     data = getUser(username)
     if 404 == data:
         return "User Not Found"
     user_data = data[1]
-    dwg = svg(user_data)
-    return Response(content = dwg.tostring(),media_type='image/svg+xml')
+    if user_data:
+        dwg = svg(user_data)
+        return Response(content = dwg.tostring(),media_type='image/svg+xml')
+    return "Error Creating kaggle card"
