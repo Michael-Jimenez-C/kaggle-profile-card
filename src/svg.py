634b3b08858620514b37f87b138067e6daf3b5c7
Michael Jimenez <michaeljimenezcontreras1@gmail.com>
2024-09-19 21:38:07 -0500

    Moví variables por constantes para mejorar el control sobre el diseño se separó la logica de la construccion de la imagen de perfil a una función separada

--- src/svg.py
+++ src/svg.py
@@ -10,7 +10,7 @@ COLORS ={
         "GRANDMASTER":"#fae041"
         }
 
-ARCS = { 
+ARCS = {
         "NOVICE":"M 70 15 A 55 55 0 0 1 122.31 53",
         "CONTRIBUTOR":"M 70 15 A 55 55 0 0 1 102.33 114.5",
         "EXPERT":"M 70 15 A 55 55 0 1 1 37.67 114.5",
@@ -18,44 +18,53 @@ ARCS = {
         "GRANDMASTER":"M 70 15 A 55 55 0 1 1 69.9 15"
         }
 
-def svg(data: dict):
-    
-    width = 500
+WIDTH = 500
+IMAGE_XY = (20,20)
+IMAGE_BORDER_CENTER = [i+50 for i in IMAGE_XY]
+RADIUS = 55
 
+
+def image(image_url, tier):
+    dwg = svgwrite.Drawing()
+    dwg.add(dwg.image(f"static/{tier.lower()}.svg", insert = (WIDTH-70, 20), size=(50,50)))
+    profile_image = requests.get(image_url)
+    img_data = base64.b64encode(profile_image.content).decode('utf-8')
+
+    dwg.add(dwg.circle(center = IMAGE_BORDER_CENTER, r = RADIUS, fill = "#1c1d20", stroke = 'gray', stroke_width=3))
+
+    dwg.add(dwg.path(d=ARCS[tier], fill="none", stroke=COLORS[tier], stroke_width=3))
+
+    mask = dwg.mask(id = 'perfil')
+    circle = dwg.circle(center = IMAGE_BORDER_CENTER, r = RADIUS-5, fill ="#ffffff")
+    mask.add(circle)
+    dwg.add(mask)
+
+    dwg.add(dwg.image(f"data:image/png;base64,{img_data}", insert = IMAGE_XY, size=(100,100), mask = "url(#perfil)"))
+
+    return dwg
+
+def svg(data: dict):
+    """
+    This function is used to create the svg usign the data from the scrapper, like image, achievements, and tier.
+    """
     profile = data['userProfile']
     image_url = profile['userAvatarUrl']
     if 'static' in image_url:
         image_url = 'https://www.kaggle.com'+image_url
-    # imagen
-    profile_image = requests.get(image_url)
-    img_data = base64.b64encode(profile_image.content).decode('utf-8')
-    
     # dibujo
-    
+
     tier = 'NOVICE' if 'performanceTier' not in profile else profile['performanceTier']
 
+
+
     dwg = svgwrite.Drawing()
-    dwg.add(dwg.rect(insert=(0,0), size = (width,200), rx=40,ry=40, fill = "#1c1d20",stroke=COLORS[tier], stroke_width=3))
-    dwg.add(dwg.image(f"https://www.kaggle.com/static/images/tiers/{tier.lower()}.svg", insert = (width-70, 20), size=(50,50)))
+    dwg.add(dwg.rect(insert=(0,0), size = (WIDTH,200), rx=40,ry=40, fill = "#1c1d20",stroke=COLORS[tier], stroke_width=3))
 
-    # Image de perfil
-    
-    dwg.add(dwg.circle(center = (70,70), r = 55, fill = "#1c1d20", stroke = 'gray', stroke_width=3))
-
-    dwg.add(dwg.path(d=ARCS[tier],fill="none", stroke=COLORS[tier], stroke_width=3))
-
-    mask = dwg.mask(id = 'perfil')
-    circle = dwg.circle(center = (70,70), r = 50, fill ="#ffffff")
-    mask.add(circle)
-    dwg.add(mask)
-
-    dwg.add(dwg.image(f"data:image/png;base64,{img_data}", insert = (20,20), size=(100,100), mask = "url(#perfil)"))
-
-    
+    dwg.add(image(image_url, tier))
     # Datos del perfil
     dwg.add(dwg.text(profile['displayName'], insert = (150,50), fill='white'))
     dwg.add(dwg.text(profile['userName'], insert = (150, 80), fill = 'white'))
-    
+
     for i in "Competitions Kernels Datasets Discussions".split():
         if "total"+i not in profile:
             profile["total"+i] = 0
